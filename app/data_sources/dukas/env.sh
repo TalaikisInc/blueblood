b2c1d3ad0f406df5ffbfa065f73773a46b8af822
Tadas Talaikis <xenu256@users.noreply.github.com>
2018-07-03 23:33:24 +0300

    add fred

--- app/data_sources/dukas/env.sh
+++ app/data_sources/dukas/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+go get github.com/Adyzng/go-duka
 go get github.com/adyzng/go-duka/...
 go build
 go-duka -format csv -start "2016-01-01" -symbol EURUSD -timeframe M15 -verbose -header -output "../../../storage"
--- app/data_sources/fred/index.py
+++ app/data_sources/fred/index.py
@@ -1,17 +1,23 @@
 from os import getenv
 from os.path import join, dirname, abspath
+from os.path import abspath, join, dirname
+import sys
+sys.path.append(abspath(join(dirname(dirname(__file__)), "../..")))
 
 from dotenv import load_dotenv
-load_dotenv(dotenv_path=join(dirname(dirname(dirname(abspath(__file__)))), '.env'))
+load_dotenv(dotenv_path=join(dirname(dirname(dirname(dirname(abspath(__file__))))), '.env'))
 from fredapi import Fred
 
-from variables.data_keywords import FRED_KEYWORDS
+from variables.data import FRED_KEYWORDS
+
 
 fred = Fred(api_key=getenv("FRED_API_KEY"))
+base_path = join(dirname(dirname(dirname(abspath(__file__)))), "storage", "fred")
 
 def get(keyword):
-    data = fred.get_series_all_releases(keyword)
-    print(data)
+    data = fred.get_series_all_releases(keyword[0])
+    data.set_index('date', inplace=True)
+    data.to_pickle(join(base_path, "{0}.p".format(keyword[0])))
 
 def run():
     for keyword in FRED_KEYWORDS:
