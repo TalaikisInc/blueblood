20b2222d45714f5cee2e78cb341a7d8058a0eec4
Tadas Talaikis <xenu256@users.noreply.github.com>
2018-07-05 17:39:15 +0300

    latest

---
+++ app/data/edgar/index.py
@@ -0,0 +1,7 @@
+from SECEdgar.crawler import SecCrawler
+crawler = SecCrawler()
+
+
+def get(symbol):
+    crawler.filing_10Q(symbol, '0000320193', '20010101', '10')
+# http://rankandfiled.com/#/data/tickers
