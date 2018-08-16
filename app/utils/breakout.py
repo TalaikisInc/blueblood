c56960102c6200a8ad1ff5487eaec98ddd20a8ac
Tadas Talaikis <xenu256@users.noreply.github.com>
2018-08-16 20:46:21 +0300

    fix breakout

--- app/utils/breakout.py
+++ app/utils/breakout.py
@@ -6,8 +6,8 @@ def breakout(df, col, op, per):
 
     if op == 'up':
         df['mx'] = df[col].rolling(window=per, min_periods=per).max()
-        df['sig'] = where(df[col].shift() > df['mx'], 1, 0)
+        df['sig'] = where(df[col] > df['mx'].shift(), 1, 0)
     if op == 'dn':
         df['mn'] = df[col].rolling(window=per, min_periods=per).min()
-        df['sig'] = where(df[col].shift() < df['mn'], 1, 0)
+        df['sig'] = where(df[col] < df['mn'].shift(), 1, 0)
     return df['sig']
